124b9e71da743d59028f0dcd62806d61be1081f8
Jesus <jesus@mediainjection.com>
2014-12-07 21:49:21 +0100

    readme changes

--- stocks/README.md
+++ stocks/README.md
@@ -1,1 +1,10 @@
 App to manage simple technical analisys for the most common stock markets.
+
+Created with "yo angular-require"
+
+
+TODO LIST
+==
+
+* Add theme:  xenon theme http://themeforest.net/item/xenon-bootstrap-admin-theme-with-angularjs/9059661
+* Create the list of the operatives markets
--- stocks/test/test-main.js
+++ stocks/test/test-main.js
@@ -12,22 +12,7 @@ requirejs.config({
     // Karma serves files from '/base'
     baseUrl: '/base/app/scripts',
 
-    paths: {
-    angular: '../../bower_components/angular/angular',
-    'angular-animate': '../../bower_components/angular-animate/angular-animate',
-    'angular-cookies': '../../bower_components/angular-cookies/angular-cookies',
-    'angular-mocks': '../../bower_components/angular-mocks/angular-mocks',
-    'angular-resource': '../../bower_components/angular-resource/angular-resource',
-    'angular-route': '../../bower_components/angular-route/angular-route',
-    'angular-sanitize': '../../bower_components/angular-sanitize/angular-sanitize',
-    'angular-scenario': '../../bower_components/angular-scenario/angular-scenario',
-    'angular-touch': '../../bower_components/angular-touch/angular-touch',
-    bootstrap: '../../bower_components/bootstrap/dist/js/bootstrap',
-    material: '../../bower_components/bootstrap-material-design/dist/js/material.min',
-    ripples: '../../bower_components/bootstrap-material-design/dist/js/ripples.min',
-    jquery: '../../bower_components/jquery/dist/jquery',
-    'flat-ui': '../../bower_components/flat-ui/dist/js/flat-ui'
-  },
+    ,
 
     shim: {
         'angular' : {'exports' : 'angular'},
